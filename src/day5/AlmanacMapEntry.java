80f5de154fb77f02ac622bf9e7346dadff727d4f
F Heath-Renn <fheathrenn@gmail.com>
2023-12-05 12:37:39 +0000

    Slow day 5 star 2 solution

--- src/day5/AlmanacMapEntry.java
+++ src/day5/AlmanacMapEntry.java
@@ -2,11 +2,23 @@ package day5;
 
 import java.util.Optional;
 
-public record AlmanacMapEntry(long destinationRangeStart, long sourceRangeStart, long rangeLength) {
+public record AlmanacMapEntry(long destinationRangeStart, long sourceRangeStart, long rangeLength) implements Comparable<AlmanacMapEntry> {
     public Optional<Long> convert(long source) {
         if (source < sourceRangeStart || source >= sourceRangeStart + rangeLength) {
             return Optional.empty();
         }
         return Optional.of(source - sourceRangeStart + destinationRangeStart);
+    }
+
+    public Optional<Long> backConvert(long destination) {
+        if (destination < destinationRangeStart || destination >= destinationRangeStart + rangeLength) {
+            return Optional.empty();
+        }
+        return Optional.of(destination - destinationRangeStart + sourceRangeStart);
+    }
+
+    @Override
+    public int compareTo(AlmanacMapEntry o) {
+        return Long.compare(this.sourceRangeStart, o.sourceRangeStart);
     }
 }
