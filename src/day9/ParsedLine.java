6fe7c8026630c3245a29b76837338639156bd358
F Heath-Renn <fheathrenn@gmail.com>
2023-12-10 02:20:47 +0000

    Day 9 stars 1 and 2

---
+++ src/day9/ParsedLine.java
@@ -0,0 +1,26 @@
+package day9;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class ParsedLine {
+
+    // State
+    List<Integer> integers = new ArrayList<>();
+
+    private static final Pattern INT_PATTERN = Pattern.compile("-?\\d+");
+
+    // Parsing
+    public ParsedLine(String line) {
+        Matcher matcher = INT_PATTERN.matcher(" " + line);
+        matcher.matches();
+        while (matcher.find()) {
+            integers.add(Integer.parseInt(matcher.group()));
+        }
+        Collections.reverse(integers); // this line is the difference between part 1 and part 2
+    }
+}
