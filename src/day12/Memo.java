fb4d88f900685718a2b65ae19cc3a73976670fd2
F Heath-Renn <fheathrenn@gmail.com>
2023-12-12 12:06:56 +0000

    Day 12 final cleanup

--- src/day12/Memo.java
+++ src/day12/Memo.java
@@ -1,5 +1,6 @@
 package day12;
 
+import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +12,7 @@ public class Memo {
 
     public static Optional<Long> lookup(String pattern, List<Integer> groupDefinition) {
         String reducedPattern = pattern.replaceAll("\\.+", ".");
-        String key = reducedPattern + ";" + groupDefinition.hashCode();
+        String key = reducedPattern + ";" + groupDefinition.toString();
         if (memo.containsKey(key)) {
             return Optional.of(memo.get(key));
         }
@@ -20,8 +21,18 @@ public class Memo {
 
     public static void store(String pattern, List<Integer> groupDefinition, long combinations) {
         String reducedPattern = pattern.replaceAll("\\.+", ".");
-        String key = reducedPattern + ";" + groupDefinition.hashCode();
+        String key = reducedPattern + ";" + groupDefinition.toString();
         memo.put(key, combinations);
+    }
+
+    public static void dump() {
+        try (FileWriter writer = new FileWriter("src/day12/output.txt")) {
+            for (Map.Entry<String, Long> entry : memo.entrySet()) {
+                writer.append(entry.getKey() + " : " + entry.getValue() + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
 }
