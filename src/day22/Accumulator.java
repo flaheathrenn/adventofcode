72afa7d6e0affcf5d3fb7a504b2fa7698a8bdf00
F Heath-Renn <fheathrenn@gmail.com>
2023-12-24 01:38:24 +0000

    Day 22: some code that works for the example for star 1 but gives an answer too high on the actual input

---
+++ src/day22/Accumulator.java
@@ -0,0 +1,102 @@
+package day22;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Accumulator {
+    // State
+    Set<Brick> bricks = new TreeSet<>();
+
+    int maxX = 0;
+    int maxY = 0;
+    int maxZ = 0;
+
+    // Update state from parsed line
+    public Accumulator update(Brick brick) {
+        bricks.add(brick);
+        maxX = Integer.max(maxX, Integer.max(brick.startX, brick.endX));
+        maxY = Integer.max(maxY, Integer.max(brick.startY, brick.endY));
+        maxZ = Integer.max(maxZ, Integer.max(brick.startZ, brick.endZ));
+        return this;
+    }
+
+    // Extract solution
+    public String star1() {
+        Brick[][][] brickArena = new Brick[maxX + 1][maxY + 1][maxZ + 1];
+
+        for (Brick brick : bricks) {
+            // drop into the 'brick arena'
+            // bricks is sorted by minimum Z, so no later brick will ever be lower than this
+            // one,
+            // so we can examine the spaces under it
+            dropBrick(brick, brickArena);
+        }
+
+        int freeBricks = 0;
+        for (Brick brick : bricks) {
+            if (brick.supportedBricks.isEmpty()) {
+                freeBricks++;
+                continue;
+            }
+            for (Brick supportedBrick : brick.supportedBricks) {
+                if (supportedBrick.supportingBricks.size() > 1) {
+                    freeBricks++;
+                    break;
+                }
+            }
+        }
+        return String.valueOf(freeBricks);
+    }
+
+    /**
+     * Mutates brickArena by creating brick brick and then letting it fall.
+     * <p>
+     * <p>
+     * First, brick is created at its listed coordinates, and then it falls until it
+     * is supported by either
+     * bricks or the floor.
+     * The coordinates in brickArena in which it comes to rest are all update with a
+     * reference to brick.
+     * Brick is then updated to add any supporting bricks to its supportingBricks
+     * property,
+     * and those supporting bricks are updated to add brick to their supportedBricks
+     * property.
+     */
+    private void dropBrick(Brick brick, Brick[][][] brickArena) {
+        // examine each layer starting from one lower than this brick's bottom
+        for (int z = brick.lowestZ() - 1; z > 0; z--) {
+            boolean brickSupported = false;
+            // examine each cell in the XY plane below this brick
+            for (int y = brick.startY; y < brick.endY + 1; y++) {
+                for (int x = brick.startX; x < brick.endX + 1; x++) {
+                    if (brickArena[x][y][z] != null) {
+                        Brick supportingBrick = brickArena[x][y][z];
+                        brick.supportingBricks.add(supportingBrick);
+                        supportingBrick.supportedBricks.add(brick);
+                        brickSupported = true;
+                    }
+                }
+            }
+            // mark the cells this brick occupies
+            if (brickSupported) {
+                for (int y = brick.startY; y < brick.endY + 1; y++) {
+                    for (int x = brick.startX; x < brick.endX + 1; x++) {
+                        for (int height = 0; height < brick.endZ - brick.startZ + 1; height++) {
+                            brickArena[x][y][z + 1 + height] = brick;
+                        }
+                    }
+                }
+                return;
+            }
+        }
+        // brick landed on floor
+        for (int y = brick.startY; y < brick.endY + 1; y++) {
+            for (int x = brick.startX; x < brick.endX + 1; x++) {
+                brickArena[x][y][1] = brick;
+                for (int height = 0; height < brick.endZ - brick.startZ + 1; height++) {
+                    brickArena[x][y][1 + height] = brick;
+                }
+            }
+        }
+    }
+}
---
+++ src/day22/Brick.java
@@ -0,0 +1,52 @@
+package day22;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Brick implements Comparable<Brick> {
+    final static Pattern regex = Pattern.compile("(\\d+),(\\d+),(\\d+)~(\\d+),(\\d+),(\\d+)");
+
+    // State
+    final int startX;
+    final int startY;
+    final int startZ;
+    final int endX;
+    final int endY;
+    final int endZ;
+
+    final Set<Brick> supportedBricks = new HashSet<>();
+    final Set<Brick> supportingBricks = new HashSet<>();
+
+    // Parsing
+    public Brick(String line) {
+        // 1,0,1~1,2,1
+        Matcher m = regex.matcher(line);
+        m.find();
+        startX = Integer.parseInt(m.group(1));
+        startY = Integer.parseInt(m.group(2));
+        startZ = Integer.parseInt(m.group(3));
+        endX = Integer.parseInt(m.group(4));
+        endY = Integer.parseInt(m.group(5));
+        endZ = Integer.parseInt(m.group(6));
+    }
+
+    public int lowestZ() {
+        return Integer.min(startZ, endZ);
+    }
+
+    @Override
+    public int compareTo(Brick o) {
+        int comparisonResult = Integer.compare(this.lowestZ(), o.lowestZ());
+        if (comparisonResult != 0) {
+            return comparisonResult;
+        }
+        comparisonResult = Integer.compare(this.startY, o.startY);
+        if (comparisonResult != 0) {
+            return comparisonResult;
+        }
+        return Integer.compare(this.startX, o.startX);
+    }
+
+}
