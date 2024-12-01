a8649ae4d8be2081b07d5877f5cbdc769e4bfe23
F Heath-Renn <fheathrenn@gmail.com>
2024-12-01 12:45:33 +0000

    Day 1 solution, not very optimised or anything but it works

--- src/aoc2024/day1/Accumulator.java
+++ src/aoc2024/day1/Accumulator.java
@@ -1,15 +1,40 @@
 package aoc2024.day1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class Accumulator {
     // State
+    List<Long> lefts = new ArrayList<>();
+    List<Long> rights = new ArrayList<>();
 
     // Update state from parsed line
     public Accumulator update(ParsedLine parsedLine) {
-        return new Accumulator();
+        lefts.add(parsedLine.left);
+        rights.add(parsedLine.right);
+        return this;
     }
 
     // Extract solution
     public String star1() {
-        return "";
+        Collections.sort(lefts);
+        Collections.sort(rights);
+        Iterator<Long> leftIt = lefts.iterator();
+        Iterator<Long> rightIt = rights.iterator();
+        long acc = 0;
+        while (leftIt.hasNext()) {
+            acc += Math.abs(leftIt.next() - rightIt.next());
+        }
+        return Long.toString(acc);
+    }
+
+    public String star2() {
+        long acc = 0;
+        for (long left : lefts) {
+            acc += left * rights.stream().filter(x -> left == x).count();
+        }
+        return Long.toString(acc);
     }
 }
--- src/aoc2024/day1/ParsedLine.java
+++ src/aoc2024/day1/ParsedLine.java
@@ -1,13 +1,22 @@
 package aoc2024.day1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ParsedLine {
 
     // State
+    long left;
+    long right;
 
 
     // Parsing
     public ParsedLine(String line) {
-
+        Matcher lineMatcher = Pattern.compile("^(\\d+)\\s+(\\d+)$").matcher(line);
+        lineMatcher.matches();
+        left = Long.parseLong(lineMatcher.group(1));
+        right = Long.parseLong(lineMatcher.group(2));
+        // System.out.println(String.format("Parsed %s to %d and %d", line, left, right));
     }
 
 }
