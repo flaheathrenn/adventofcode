17cff3194d6a1d5a962f4123ac9ac034e2bdd6ae
F Heath-Renn <fheathrenn@gmail.com>
2024-12-02 13:05:39 +0000

    Day 2 star 2 solution

--- src/aoc2024/day2/Accumulator.java
+++ src/aoc2024/day2/Accumulator.java
@@ -1,17 +1,57 @@
 package aoc2024.day2;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class Accumulator {
     // State
-    long safeRecordCount = 0;
+    long safeRecordCountStar1 = 0;
+    long safeRecordCountStar2 = 0;
 
     // Update state from parsed line
     public Accumulator update(ParsedLine parsedLine) {
-        safeRecordCount += parsedLine.recordSafe ? 1 : 0;
+        safeRecordCountStar1 += isRecordSafe(parsedLine.differences, false) ? 1 : 0;
+        safeRecordCountStar2 += isRecordSafe(parsedLine.differences, true) ? 1 : 0;
         return this;
     }
 
     // Extract solution
     public String star1() {
-        return Long.toString(safeRecordCount);
+        return Long.toString(safeRecordCountStar1);
+    }
+
+    public String star2() {
+        return Long.toString(safeRecordCountStar2);
+    }
+
+    private boolean isRecordSafe(List<Long> differences, boolean dampen) {
+        int majoritySign = Integer.signum(differences.stream().map(Long::signum).reduce(0, Integer::sum));
+        Optional<Long> firstUnsafeDifference = differences.stream().filter(difference -> {
+            return Long.signum(difference) != majoritySign || Math.abs(difference) > 3;
+        }).findFirst();
+        if (firstUnsafeDifference.isEmpty()) {
+            return true;
+        }
+        if (!dampen) {
+            return false;
+        }
+        int firstUnsafeDifferenceIndex = differences.indexOf(firstUnsafeDifference.get());
+        if (firstUnsafeDifferenceIndex == differences.size() - 1) {
+            return true; // i.e. only the last level difference is unsafe, so removing the last level will dampen the problem out
+        }
+        if (firstUnsafeDifferenceIndex == 0) {
+            // if the first difference is unsafe, it might be the case that the first level is the only unsafe one, so test that:
+            if (isRecordSafe(differences.subList(1, differences.size()), false)) {
+                return true;
+            }
+            // it could still be the case that the second level is the unsafe one so proceed
+        }
+        long combinedNewDifference = differences.remove(firstUnsafeDifferenceIndex); // remove unsafe difference
+        combinedNewDifference += differences.remove(firstUnsafeDifferenceIndex); // and the one after
+        differences.add(firstUnsafeDifferenceIndex, combinedNewDifference); // and replace with their combined value
+        boolean newValue = isRecordSafe(differences, false);
+        return newValue;
+
     }
 }
--- src/aoc2024/day2/ParsedLine.java
+++ src/aoc2024/day2/ParsedLine.java
@@ -1,42 +1,29 @@
 package aoc2024.day2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ParsedLine {
 
     // State
-    boolean recordSafe = false;
+    List<Long> differences = new ArrayList<>();
 
     // Parsing
     public ParsedLine(String line) {
         Pattern p = Pattern.compile("\\d+");
         Matcher m = p.matcher(line);
         Long lastNumber = null;
-        Boolean isIncreasing = null;
         while (m.find()) {
             if (lastNumber == null) {
                 lastNumber = Long.parseLong(m.group());
                 continue;
             }
             long thisNumber = Long.parseLong(m.group());
-            if (thisNumber == lastNumber) { // record unsafe
-                return;
-            }
-            if (isIncreasing == null) {
-                isIncreasing = thisNumber > lastNumber;
-            } else {
-                if (isIncreasing != thisNumber > lastNumber) { // record unsafe
-                    return;
-                }
-            }
-            long diff = Math.abs(thisNumber - lastNumber);
-            if (diff > 3) { // record unsafe
-                return;
-            }
+            differences.add(thisNumber - lastNumber);
             lastNumber = thisNumber;
         } // end of loop
-        recordSafe = true;
     }
 
 }
