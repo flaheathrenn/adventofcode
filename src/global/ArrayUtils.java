654bc78f8235921c5019e75f969c97451f70eea5
F Heath-Renn <fheathrenn@gmail.com>
2024-12-06 12:07:41 +0000

    Adding some new code for dealing with grids to global package since I'm likely to use it again

--- src/global/ArrayUtils.java
+++ src/global/ArrayUtils.java
@@ -1,5 +1,7 @@
 package global;
 
+import java.util.Arrays;
+
 public interface ArrayUtils {
     /**
      * Convert input's horizontal lines to vertical lines and vice versa
@@ -13,5 +15,13 @@ public interface ArrayUtils {
             }
         }
         return result;
+    }
+
+    public static <T> T[][] deepCopyOf(T[][] original) {
+        T[][] copy = Arrays.copyOf(original, original.length);
+        for (int i = 0; i < copy.length; i++) {
+            copy[i] = Arrays.copyOf(original[i], original[i].length);
+        }
+        return copy;
     }
 }
