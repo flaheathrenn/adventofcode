1d03aa84ece4c91cdcad1a7475be83ae79889e33
Frederic Heath-Renn <fheathrenn@gmail.com>
2024-12-25 09:08:19 +0000

    Ineffectual fumblings towards a couple of star 2s

--- src/aoc2023/day25/Accumulator.java
+++ src/aoc2023/day25/Accumulator.java
@@ -1,14 +1,18 @@
 package aoc2023.day25;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 public class Accumulator {
     // State
-    Set<String> nodes = new HashSet<>();
-    Set<Set<String>> connections = new HashSet<>();
+    List<String> nodes = new ArrayList<>();
+    List<Set<String>> connections = new ArrayList<>();
 
     // Update state from parsed line
     public Accumulator update(ParsedLine parsedLine) {
@@ -26,117 +30,73 @@ public class Accumulator {
         System.out.println("There are " + nodes.size() + " nodes with a total of " + connections.size()
                 + " connections between them");
 
-        // Map<Integer, Integer> cardinalityMap = new HashMap<>();
-        // for (String node : nodes) {
-        //     long cardinality = connections.stream().filter(c -> c.contains(node)).count();
-        //     if (!cardinalityMap.containsKey((int) cardinality)) {
-        //         cardinalityMap.put((int) cardinality, 1);
-        //     } else {
-        //         cardinalityMap.put((int) cardinality, cardinalityMap.get((int) cardinality) +  1);
-        //     }
-        // }
-        // System.out.println(cardinalityMap);
+        Map<Set<String>, Integer> connectionFrequency = new HashMap<>();
 
-        Set<String> inGroup = new HashSet<>();
-        Set<String> outGroup = new HashSet<>();
-        for (String thisNode : nodes) {
-            Map<Integer, Integer> matchingZerosCount = new HashMap<>();
-            for (String otherNode: nodes) {
-                if (thisNode.equals(otherNode)) {
-                    continue;
-                }
-                int matchingZeros = 0;
-                for (String node : nodes) {
-                    if (node.equals(thisNode) || node.equals(otherNode)) {
-                        continue;
-                    }
-                    if (!connections.contains(Set.of(thisNode, node)) && !connections.contains(Set.of(otherNode, node))) {
-                        matchingZeros++;
-                    }
-                }
-                matchingZerosCount.put(matchingZeros, matchingZerosCount.containsKey(matchingZeros) ? matchingZerosCount.get(matchingZeros) + 1 : 1);
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            System.out.print(".");
+            String node = nodes.get(random.nextInt(nodes.size() - 1));
+            String otherNode = nodes.get(random.nextInt(nodes.size() - 1));
+            if (node.equals(otherNode)) {
+                continue;
             }
-            int lows = matchingZerosCount.get(1538) + matchingZerosCount.get(1539);
-            int highs = matchingZerosCount.get(1540);
-            if (lows < highs) {
-                inGroup.add(thisNode);
-            } else {
-                outGroup.add(thisNode);
-            }
-
-            // have prospective groups
-            for (String igNode : inGroup) {
-                for (String ogNode : outGroup) {
-                    if (connections.contains(Set.of(igNode, ogNode))) {
-                        System.out.println("HIT");
-                    }
+            try {
+                Set<Set<String>> path = findPath(node, otherNode, Collections.emptySet());
+                for (Set<String> connection : path) {
+                    connectionFrequency.put(connection,
+                            connectionFrequency.containsKey(connection) ? connectionFrequency.get(connection) + 1
+                                    : 1);
                 }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         }
 
-        return Integer.toString(inGroup.size() * outGroup.size());
+        List<Map.Entry<Set<String>, Integer>> entrySetSorted = connectionFrequency.entrySet().stream()
+                .sorted((e1, e2) -> Integer.compare(e1.getValue(), e2.getValue())).toList();
+        for (Map.Entry<Set<String>, Integer> entry : entrySetSorted) {
+            System.out.println("Connection " + entry.getKey() + " used " + entry.getValue() + " times");
+        }
 
-        // Set<String> inGroup = Set.of("bvb", "hfx", "jqt", "ntq", "rhn", "xhk");
-        // List<String> nodesOrdered = nodes.stream().sorted((s1, s2) -> {
-        //     if (inGroup.contains(s1) && !inGroup.contains(s2)) {
-        //         return -1;
-        //     }
-        //     if (!inGroup.contains(s1) && inGroup.contains(s2)) {
-        //         return 1;
-        //     }
-        //     return s1.compareTo(s2);
-        // }).toList();
-        // System.out.println(nodesOrdered);
-        // for (String me : nodesOrdered) {
-        //     for (String you : nodesOrdered) {
-        //         if (me.equals(you)) {
-        //             System.out.print("-");
-        //         } else {
-        //             System.out.print(connections.contains(Set.of(me, you)) ? "1" : "0");
-        //         }
-        //     }
-        //     System.out.println();
-        // }
-
-        // List<Set<String>> connectionsSorted = new ArrayList<>(connections);
-        // for (Set<String> connection1 : connectionsSorted) {
-        //     for (Set<String> connection2 : connectionsSorted) {
-        //         if (connection1.equals(connection2)) {
-        //             continue;
-        //         }
-        //         for (Set<String> connection3 : connectionsSorted) {
-        //             if (connection3.equals(connection1) || connection3.equals(connection2)) {
-        //                 continue;
-        //             }
-        //             Set<Set<String>> reducedConnections = new HashSet<>(connections);
-        //             reducedConnections.remove(connection1);
-        //             reducedConnections.remove(connection2);
-        //             reducedConnections.remove(connection3);
-        //             int maxReachable = maxReachable(nodes, reducedConnections);
-        //             if (maxReachable(nodes, reducedConnections) != nodes.size()) {
-        //                 return Integer.toString(maxReachable * (nodes.size() - maxReachable));
-        //             }
-        //         }
-        //     }
-        // }
-        // return "";
+        return "";
     }
 
-    // private int maxReachable(Set<String> allNodes, Set<Set<String>> reducedConnections) {
-    //     String startNode = allNodes.stream().findAny().orElseThrow(IllegalStateException::new);
-    //     Set<String> visitedNodes = new HashSet<>();
-    //     Queue<String> nodesToVisit = new LinkedList<>();
-    //     nodesToVisit.add(startNode);
-    //     while (nodesToVisit.size() != 0) {
-    //         String visit = nodesToVisit.poll();
-    //         visitedNodes.add(visit);
-    //         reducedConnections.stream().filter(c -> c.contains(visit)).forEach(c -> {
-    //             String other = c.stream().filter(n -> !visit.equals(n)).findAny().orElseThrow(IllegalStateException::new);
-    //             if (!visitedNodes.contains(other)) {
-    //                 nodesToVisit.add(other);
-    //             }
-    //         });
-    //     }
-    //     return visitedNodes.size();
-    // }
+    private Set<Set<String>> findPath(String node, String otherNode, Set<Set<String>> usedConnections) {
+        Collections.shuffle(connections);
+        for (Set<String> connection : connections) {
+            if (!connection.contains(node)) {
+                continue;
+            }
+            if (usedConnections.contains(connection)) {
+                continue;
+            }
+            if (connection.contains(otherNode)) {
+                // found connection
+                return Set.of(connection);
+            } else {
+                String intermediateNode = connection.stream().filter(n -> !n.equals(node)).findAny()
+                        .orElseThrow(IllegalStateException::new);
+                Set<Set<String>> path = new HashSet<>();
+                path.add(connection);
+                if (path.size() > 10) {
+                    break;
+                }
+                Set<Set<String>> newUsedConnections = new HashSet<>();
+                newUsedConnections.addAll(usedConnections);
+                newUsedConnections.add(connection);
+                try {
+                    Set<Set<String>> restOfPath = findPath(intermediateNode, otherNode, newUsedConnections);
+                    if (restOfPath.isEmpty()) {
+                        continue;
+                    } else {
+                        path.addAll(restOfPath);
+                        return path;
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+        return Collections.emptySet();
+    }
 }
--- src/aoc2024/day24/Accumulator.java
+++ src/aoc2024/day24/Accumulator.java
@@ -3,15 +3,18 @@ package aoc2024.day24;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 
 public class Accumulator {
     // State
     Map<String, Boolean> wireValues = new HashMap<>();
     Queue<Gate> gateQueue = new LinkedList<>();
+    Map<String, Gate> gatesControllingWire = new HashMap<>();
 
     // Update state from parsed line
     public Accumulator update(ParsedLine parsedLine) {
@@ -21,6 +24,7 @@ public class Accumulator {
             wireValues.put(parsedLine.wire, parsedLine.wireValue);
         } else if (parsedLine.isGate) {
             gateQueue.add(parsedLine.gate);
+            gatesControllingWire.put(parsedLine.gate.outputWire(), parsedLine.gate);
         }
         return this;
     }
@@ -36,16 +40,108 @@ public class Accumulator {
 
         // System.out.println(wireValues);
 
-        List<String> zWires = new ArrayList<>(wireValues.keySet().stream().filter(x -> x.startsWith("z")).sorted().toList());
-        Collections.reverse(zWires);
-        long solution = 0L;
-        for (String zWire : zWires) {
-            // System.out.println(zWire + ": " + wireValues.get(zWire));
-            solution *= 2;
-            if (Boolean.TRUE.equals(wireValues.get(zWire))) {
-                solution++;
+        // List<String> xWires = new ArrayList<>(
+        // wireValues.keySet().stream().filter(x ->
+        // x.startsWith("x")).sorted().toList());
+        // Collections.reverse(xWires);
+        // long xValue = 0L;
+        // for (String xWire : xWires) {
+        // xValue *= 2;
+        // if (Boolean.TRUE.equals(wireValues.get(xWire))) {
+        // xValue++;
+        // }
+        // }
+
+        // List<String> yWires = new ArrayList<>(
+        // wireValues.keySet().stream().filter(x ->
+        // x.startsWith("y")).sorted().toList());
+        // Collections.reverse(yWires);
+        // long yValue = 0L;
+        // for (String yWire : yWires) {
+        // yValue *= 2;
+        // if (Boolean.TRUE.equals(wireValues.get(yWire))) {
+        // yValue++;
+        // }
+        // }
+
+        // List<String> zWires = new ArrayList<>(
+        // wireValues.keySet().stream().filter(x ->
+        // x.startsWith("z")).sorted().toList());
+        // Collections.reverse(zWires);
+        // long solution = 0L;
+        // for (String zWire : zWires) {
+        // // System.out.println(zWire + ": " + wireValues.get(zWire));
+        // solution *= 2;
+        // if (Boolean.TRUE.equals(wireValues.get(zWire))) {
+        // solution++;
+        // }
+        // }
+
+        // System.out.println("x value: " + Long.toBinaryString(xValue));
+        // System.out.println("y value: " + Long.toBinaryString(yValue));
+        // System.out.println("z value: " + Long.toBinaryString(solution));
+        // System.out.println("expected z value: " + Long.toBinaryString(xValue +
+        // yValue));
+        // System.out.println("errors: " + Long.toBinaryString(solution ^ (xValue +
+        // yValue)));
+
+        // return Long.toString(solution);
+
+        // Set<Gate> gatesTo44 = gatesToWire("z44");
+        // Set<Gate> gatesTo23 = gatesToWire("z23");
+        // gatesTo44.retainAll(gatesTo23);
+        // System.out.println(gatesTo44);
+
+        List<String> zWires = new ArrayList<>(
+                wireValues.keySet().stream().filter(x -> x.startsWith("z")).sorted().toList());
+        for (int i = 0; i < 46; i++) {
+            String wireNumber = (i < 10 ? "0" : "") + Integer.toString(i);
+            Set<String> inputs = ultimateInputs("z" + wireNumber);
+            for (int j = 0; j <= i; j++) {
+                String subordinateWireNumber = (j < 10 ? "0" : "") + Integer.toString(j);
+                if (!inputs.remove("x" + subordinateWireNumber)) {
+                    System.out.println("z" + wireNumber + " should depend on x" + subordinateWireNumber + " but doesn't");
+                }
+                if (!inputs.remove("y" + subordinateWireNumber)) {
+                    System.out.println("z" + wireNumber + " should depend on y" + subordinateWireNumber + " but doesn't");
+                }
+            }
+            if (!inputs.isEmpty()) {
+                System.out.println("z" + wireNumber + " shouldn't depend on " + inputs + " but does");
             }
         }
-        return Long.toString(solution);
+
+        return "";
+    }
+
+    private Set<Gate> gatesToWire(String wire) {
+        if (!gatesControllingWire.containsKey(wire)) {
+            return Collections.emptySet();
+        }
+        Gate mainGate = gatesControllingWire.get(wire);
+        Set<Gate> result = new HashSet<>();
+        result.add(mainGate);
+        result.addAll(gatesToWire(mainGate.inputWire()));
+        result.addAll(gatesToWire(mainGate.inputWire2()));
+        return result;
+    }
+
+    private Set<String> ultimateInputs(String wire) {
+        if (!gatesControllingWire.containsKey(wire)) {
+            return Collections.emptySet();
+        }
+        Set<String> result = new HashSet<>();
+        Gate mainGate = gatesControllingWire.get(wire);
+        if (mainGate.inputWire().startsWith("x") || mainGate.inputWire().startsWith("y")) {
+            result.add(mainGate.inputWire());
+        } else {
+            result.addAll(ultimateInputs(mainGate.inputWire()));
+        }
+        if (mainGate.inputWire2().startsWith("x") || mainGate.inputWire2().startsWith("y")) {
+            result.add(mainGate.inputWire2());
+        } else {
+            result.addAll(ultimateInputs(mainGate.inputWire2()));
+        }
+        return result;
     }
 }
