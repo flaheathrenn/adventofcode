8411be7bc9d5a46767a6079063f4aba7ddd6b3b8
Frederic Heath-Renn <fheathrenn@gmail.com>
2025-12-06 14:29:25 +0000

    2025 day 6 star 2

--- src/aoc2025/day6/Accumulator.java
+++ src/aoc2025/day6/Accumulator.java
@@ -3,22 +3,25 @@ package aoc2025.day6;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Accumulator {
     // State
     List<Long>[] columns;
     long star1sol = 0;
 
+    List<String[]> rawContents = new ArrayList<>();
+
     // Update state from parsed line
     public Accumulator update(ParsedLine parsedLine) {
+        rawContents.add(parsedLine.rawContents);
         if (columns == null) {
             columns = new List[parsedLine.contents.length];
             for (int i = 0; i < columns.length; i++) {
-                columns[i] = new ArrayList<Long>();
+                columns[i] = new ArrayList<>();
             }
         }
         if (parsedLine.contents[0].matches("[+*]")) {
-
             for (int i = 0; i < columns.length; i++) {
                 switch (parsedLine.contents[i]) {
                     case "+":
@@ -43,5 +46,47 @@ public class Accumulator {
     // Extract solution
     public String star1() {
         return Long.toString(star1sol);
+    }
+
+    public String star2() {
+        long star2sol = 0;
+        String[][] grid = rawContents.toArray(new String[rawContents.size()][]);
+        String problemOperator = null;
+        List<Long> problemNumbers = new ArrayList<>();
+        for (int columnIndex = 0; columnIndex < grid[0].length; columnIndex++) {
+            boolean allBlank = true;
+            long currentNumber = 0;
+            for (int rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+                if (!grid[rowIndex][columnIndex].equals(" ")) {
+                    allBlank = false;
+                    if (grid[rowIndex][columnIndex].matches("[+*]")) {
+                        problemOperator = grid[rowIndex][columnIndex];
+                    } else {
+                        currentNumber = (currentNumber * 10) + Long.parseLong(grid[rowIndex][columnIndex]);
+                    }
+                }
+            }
+            if (!allBlank) {
+                problemNumbers.add(currentNumber);
+            }
+            if (allBlank || columnIndex == grid[0].length - 1) {
+                // end of a 'problem' so calculate
+                System.out.printf("Calculating %s of %s\n", problemOperator, problemNumbers.stream().map(String::valueOf).collect(Collectors.joining(",", "[", "]")));
+                switch (problemOperator) {
+                    case "+":
+                        star2sol += problemNumbers.stream().mapToLong(l -> l).sum();
+                        break;
+                    case "*":
+                        star2sol += problemNumbers.stream().mapToLong(l -> l).reduce(1L, (l1, l2) -> l1 * l2);
+                        break;
+                    case null:
+                    default:
+                        throw new IllegalArgumentException();
+                }
+                problemOperator = null;
+                problemNumbers.clear();
+            }
+        }
+        return Long.toString(star2sol);
     }
 }
--- src/aoc2025/day6/ParsedLine.java
+++ src/aoc2025/day6/ParsedLine.java
@@ -1,13 +1,18 @@
 package aoc2025.day6;
 
+import java.util.Arrays;
+
 public class ParsedLine {
 
     // State
     String[] contents;
+    String[] rawContents;
 
     // Parsing
     public ParsedLine(String line) {
-        contents = line.trim().split(" +", 0);
+        contents = line.trim().split(" +");
+        rawContents = line.split("");
+        System.out.println(Arrays.toString(rawContents));
     }
 
 }
