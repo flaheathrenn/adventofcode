c285499d72a119d57fbfb3f0de98816e6cf8295f
Frederic Heath-Renn <fheathrenn@gmail.com>
2025-12-07 10:59:06 +0000

    2025 day 7 tidy up a little

--- src/aoc2025/day7/Accumulator.java
+++ src/aoc2025/day7/Accumulator.java
@@ -8,23 +8,16 @@ import java.util.Set;
 public class Accumulator {
     // State
     int star1sol = 0;
-    Set<Integer> beamPositions = new HashSet<>();
     Map<Integer, Long> beams = new HashMap<>();
 
     // Update state from parsed line
     public Accumulator update(ParsedLine parsedLine) {
         if (parsedLine.sPosition != -1) {
-            beamPositions.add(parsedLine.sPosition);
             beams.put(parsedLine.sPosition, 1L);
         }
         for (int splitterPosition : parsedLine.splitterPositions) {
-            if (beamPositions.contains(splitterPosition)) {
-                star1sol++;
-                beamPositions.add(splitterPosition - 1);
-                beamPositions.add(splitterPosition + 1);
-                beamPositions.remove(splitterPosition);
-            }
             if (beams.containsKey(splitterPosition)) {
+                star1sol++;
                 beams.put(splitterPosition - 1, beams.getOrDefault(splitterPosition - 1, 0L) + beams.get(splitterPosition));
                 beams.put(splitterPosition + 1, beams.getOrDefault(splitterPosition + 1, 0L) + beams.get(splitterPosition));
                 beams.remove(splitterPosition);
