2fc36473ae2dac9a129f2a22f9cd401027dd54f7
Frederic Heath-Renn <fheathrenn@gmail.com>
2025-12-07 10:56:39 +0000

    2025 day 7 star 2

--- src/aoc2025/day7/Accumulator.java
+++ src/aoc2025/day7/Accumulator.java
@@ -1,17 +1,21 @@
 package aoc2025.day7;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Accumulator {
     // State
     int star1sol = 0;
     Set<Integer> beamPositions = new HashSet<>();
+    Map<Integer, Long> beams = new HashMap<>();
 
     // Update state from parsed line
     public Accumulator update(ParsedLine parsedLine) {
         if (parsedLine.sPosition != -1) {
             beamPositions.add(parsedLine.sPosition);
+            beams.put(parsedLine.sPosition, 1L);
         }
         for (int splitterPosition : parsedLine.splitterPositions) {
             if (beamPositions.contains(splitterPosition)) {
@@ -20,6 +24,11 @@ public class Accumulator {
                 beamPositions.add(splitterPosition + 1);
                 beamPositions.remove(splitterPosition);
             }
+            if (beams.containsKey(splitterPosition)) {
+                beams.put(splitterPosition - 1, beams.getOrDefault(splitterPosition - 1, 0L) + beams.get(splitterPosition));
+                beams.put(splitterPosition + 1, beams.getOrDefault(splitterPosition + 1, 0L) + beams.get(splitterPosition));
+                beams.remove(splitterPosition);
+            }
         }
         return this;
     }
@@ -27,5 +36,9 @@ public class Accumulator {
     // Extract solution
     public String star1() {
         return Integer.toString(star1sol);
+    }
+
+    public String star2() {
+        return beams.values().stream().reduce(Long::sum).map(String::valueOf).orElse("ERROR");
     }
 }
